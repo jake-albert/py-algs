186bb1942da25526d2c4b2f7d452af1a7013c0e1
Jake Albert <albert.jacob.m@gmail.com>
2019-02-01 18:06:25 -0500

    Reorganize

--- c16/c16p23.py
+++ c16/c16p23.py
@@ -2,7 +2,7 @@ from random import randint
 
 # c16p23
 
-# Rand7 from RandS: Implement a method rand7() given rand5().That is, given a
+# Rand7 from Rand5: Implement a method rand7() given rand5().That is, given a
 # method that generates a random number between 0 and 4 (inclusive), write a
 # method that generates a random number between 0 and 6 (inclusive). 
 
