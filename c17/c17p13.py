e6dbf1fdcde3c86e4179ab4c2260eab0ba071fd7
Jake Albert <albert.jacob.m@gmail.com>
2019-02-03 16:49:57 -0500

    Edit c17p17

--- c17/c17p13.py
+++ c17/c17p13.py
@@ -1,186 +1,263 @@
-# start
+import sys
+sys.path.append('..')
+from data_structs import Trie
 
-# this is quite a cool dynamic-programming problem. 
+# c17p17
 
-# the task is to optimize the number of unrecognized characters to be as low as possible,
-# but there is still a question to ask the interviewer about whether there is a preference
-# to favor words together or separate when the number of unrecognized characters would 
-# remain the same. 
+# Re-Space: Oh, no! You have accidentally removed all spaces, punctuation, and
+# capitalization in a lengthy document. A sentence like "I reset the computer. 
+# It still didn't boot!" became "iresetthecomputeritstilldidntboot". You'll 
+# deal with the punctuation and capitalization later; right now you need to 
+# re-insert the spaces. Most of the words are in a dictionary but a few are 
+# not. Given a dictionary (a list of strings) and the document (a string),
+# design an algorithm to unconcatenate the document in a way that minimizes 
+# the number of unrecognized characters.
+#
+# EXAMPLE:
+# Input: jesslookedjustliketimherbrother
+# Output: jess looked just like tim her brother (7 unrecognized characters)
+#         ‾‾‾‾                  ‾‾‾
 
-# As an example...say that the original sentence was the following:
-# "He opened the door and stepped out, sourcing the enemy with his handheld GPS tracker."
+##############################################################################
 
-# There are TWO cases here where the un-concatenation could have occurred differently. These
-# are "out sourcing" and "outsourcing", as well as "handheld" and "hand held". I don't really 
-# have a confident a priori assumption as to which would lead to more accurate results in 
-# written englih off the top of my head, though if I had to guess I would say it is better
-# to err on the side of making compound words as opposed to leaving them split. I say 
-# this mostly because I took longer to find a contrived verstion of out sourcing where that 
-# actually wasn't preferable, but that is not a realiable reason to be confident here. 
+# There are some important clarification questions. First, is there any 
+# meaning to an empty string in the input dictionary? A trivial "match" to an
+# empty string does not change the number of unrecognized characters and 
+# seems like a waste of time. I simply ignore any such strings in the input.
 
-# Additionally, there is an opposite version like "His hand held two marbles" where this 
-# assumption would go wrong. Obvioulsy, more advanced NLP techniques like POS-tagging 
-# could help minimize erros in a real life situation.
+# Another question is of the format of the document string. Are newlines 
+# between different lines part of the "punctuation" that was removed, or are
+# substrings that represent different lines separated by any boundaries? I 
+# assume for my solution that no line boundaries remain, so the document
+# contains only lower-case characters.
 
-# Still, good to ask an interviewer about this and discuss options.
+# Another question is whether there is any preference among multiple optimized 
+# spacing schemes. What, if anything, should break a tie when more than one 
+# option would lead to the same minimized number of unrecognized characters?
 
-# HERE is the framework taht I am going to use 
+# Compound words can give rise to this type of situation. Say the original 
+# sentence was as follows:
 
-# Also is helpful to ask what form te document comes in. For example, is it a single
-# string? Is it a bunch of strings that each represents a line, and we can be confident
-# that there are boundaries between the spaces? I am going to assume for this problem
-# that the document comes in the form of a single string of lower-case characters.
+# "He opened the door and stepped out, sourcing the enemy with his handheld 
+# GPS tracker."
 
-# In addition, I might ask that the dictionary come in the form of a TRIE rather than 
-# a hash table, for reasons that can be explained later. I will implement my own trie
-# later on to use, but for now let's assume the words are stored in a trie
+# If the dictionary contains tens of thousands of natural English words and 
+# their inflections, then there are TWO cases where unconcatenation would 
+# result in the same number of unrecognized characters. In one, creating a 
+# compound word would be incorrect ("outsourcing"), while in the other the 
+# opposite is true ("hand held").
 
-# I will use a memo that keeps track, for every index i in the string that represents 
-# the document, of TWO things. We will call this memo OPT, and have OPT[i] hold an object
-# with two attributes, "count" and "lastspace". Count refers to the optimized (minimal)
-# number of unrecognized characters for the string from indices 0 to i. Lastspace refers
-# to the index of the character that follows the most recent space symbol, or -1 if 
-# there is no previous space (this is for backtracking purposes).
+# I don't have a confident a priori assumption as to which default would 
+# lead to more accurate results in written English. I lean towards guessing
+# that creating compound words is more likely than not to be correct, but 
+# there are plenty of cases where this would be wrong ("hishandheldaredball"). 
 
-# We know that if we have the values of OPT[j] already stored for all indices 0<=j<i,
-# then OPT[i].count is equal to the MINIMUM of:
-#     OPT[i-1].count + 1              ......... when we treat this char as unrecognized
-# min OPT[x-1].count for all x that mark the start of a valid word that ends at i
+# More advanced NLP techniques like word sense disambiguation and POS-tagging 
+# could help minimize errors in a real-life application, but my solution makes 
+# as few words as possible while minimizing unrecognized characters and thus 
+# creates compound words whenever it can.
 
-# how do we keep track of ALL words that end at a given index i? one way would require 
-# knowing a maximum length of all the words in the dictionary and then subsequently 
-# checking for membership in the dictionary of all the substrings no longer than max
-# length that also end at i. Example for the string "jesshopelessly" we would attempt
-# for i = 13 (the "y") "y", "ly", "sly", and so on until we have hit the limit. But 
-# to do this would take O(maxlength**2) time in terms of concatenating strings and 
-# testing them. We will use a trie instead to store the words. 
+# Now, to the task: optimizing the number of unrecognized characters to be as
+# low as possible. This sounds like a dynamic programming problem.
 
-# Using a trie, then on every turn for a new index i we must do O(1) checks for 
-# every prefix that is currently possible, either extending it or not, and then if it 
-# can be extedned to the current index, then identifying whether it is the end of a 
-# word or not and updating based on that. The maximum number of prefix candidates we 
-# will be looking at for a given index is equal to the number of words that can end 
-# at an index, which naively is in O(i) since we have to check all the way back to the 
-# start, but using a trie we can cull these down to be bounded by the maxlength, and 
-# that is a VERY generous bound as using the trie will help to cull the vast majoirty
-# of naive preix guesses. It can be argued that this is a linear time algorithm
-# if the max length is considered a constant.  
+# I use a memo that keeps track of TWO things, for every index i in doc. I 
+# call this memo OPT, and have OPT[i] hold an object with two attributes, 
+# "count" and "nextword". Count refers to the optimized (minimal) number of 
+# unrecognized characters for the substring from indices i to the end of doc.
+# nextword refers to the index of the next character that should be printed
+# with a space BEFORE it (or the length of doc if no more spaces are needed) 
+# should i be treated as the start of a word.
 
-# for the purposes of memoization, we also use a special object that stores two values
+# We populate this memo from "back" to "front", with the base case 
+
+# Assuming that if we have the values of OPT[j] already stored for all indices
+# i < j < len(doc), then OPT[i].count is equal to the MINIMUM of:
+
+#     OPT[i+1].count + 1 (treat the character here as unrecognized)          
+#     min OPT[x+1].count, for all x that mark the end of a word starting at i
+
+# One way to test each of the x values is to, given a maxlength M of words in 
+# the list, check every substring from doc[i:i+1] to doc[i:i+maxlength+1].
+# Assuming that the words in the list are hashed into a Python dictionary, 
+# we could do this in O(M^2) time for each index i in doc. 
+
+# But this approach duplicates a lot of work, so I use a trie instead to store
+# words in the dictionary. This means that all we must do at each i is to 
+# traverse at most maxlength steps through the trie, checking for OPT count 
+# values any time that the trie signals an end of word. Total runtime is from 
+# loading the trie with all words in the dictionary, and processing as above,
+# and creating the output string. This is O(M*D+M*S), where D is the number of
+# entries in the dictionary and S the length of the document.
+
+# Assuming that we are working with natural English text, we might be able to
+# argue that M is in O(1) (given that we cannot expect there to be words
+# greater than 20 or so characters in a dictionary, unless perhaps we are 
+# working in some specialized domain like medicine), but for abstract cases, 
+# using a trie is asymptotically more efficient than the first approach.
+
 class OptObject:
-
-    def __init__(self,count,lastspace):
-        self.count = count
-        self.lastspace = lastspace
-
-from queue import DequeQueue
-from stack import Stack
+    """Storage class for the dynamic programming memo.
     
-# determine the optimum spacings, then print
-def f1(doc,trie):
+    Attributes:
+        count: An int. The minimal number of unrecognized characters.
+        nextword: An int. Index to start of next word. 
+    """
 
-    # an empty string is unconcatenated to an empty string
+    def __init__(self,count=None,nextword=None):
+        """Inits an OptObject with specified values, or None."""
+        self.count = count
+        self.nextword = nextword
+     
+def f1(doc,dictionary):
+    """Unconcatenates a document to minimize unrecognized characters.
+    
+    Args:
+        doc: A string of only lower-case letters.
+        dictionary: A list of strings to be treated as words. Strings 
+          should be stripped of non-letter characters. Case-insensitive. 
+    
+    Returns:
+        A string.
+    """
+    trie = Trie()
+    for word in dictionary:
+        if len(word) > 0 and len(word) <= len(doc):
+            trie.insert(word.lower())
+
+    # Both an empty string and a string of length one cannot be 
+    # unconcatenated, so return a copy of that string.
+    
     if len(doc) == 0:
         return ""
-
-    opt = get_opt(doc,trie)
-    spaces = Stack()
+    if len(doc) == 1:
+        return doc[:]
+        
+    opt = get_opt(doc,trie.root)       
+    return build_string(opt,doc)     
     
-    # use last spaces as backpointers
-    j = len(doc) - 1
-    while opt[j].lastspace >= 0:
-        spaces.push(opt[j].lastspace)
-        j = opt[j].lastspace
-        
-    # print with spaces
-    nextspace = spaces.pop()
-    for i in range(len(doc)):
-        print(doc[i],end="")
-        if i == nextspace: 
-            print(" ",end="")
-            nextspace = spaces.pop()
-    print("\n",end="")
+def get_opt(doc,root):
+    """Determines the optimal spacing for a string.
     
-# the main algorithm is written iteratively    
-def get_opt(doc,trie):
-        
-    # we prepare the memo with default values
-    opt = [OptObject(None,None) for _ in range(len(doc))] 
+    Args:
+        doc: A string of only lower-case characters.
+        root: A TrieNode instance representing the root of a trie.
     
-    # we also use a queue to keep track of currently valid prefixes
-    prefixes = DequeQueue()
-    
-    # the "base" case for the first character
-    if doc[0] in trie.root.children:
-        
-        prefixes.add(trie.root.children[doc[0]])
-        
-        if trie.root.children[doc[0]].is_end_of_word():
-            opt[0].count = 0
-            opt[0].lastspace = -1
-            if len(doc) > 1:
-                opt[1].lastspace = 0
-        else:
-            opt[0].count = 1
-            opt[0].lastspace = -1
-    else:
-        opt[0].count = 1
-        opt[0].lastspace = -1
-        
-    # the "recursive" case that uses previous values
-    for i in range(1,len(doc)):
-        
-        # the default value for the optimal count is if we discard the current character
-        opt[i].count = opt[i-1].count+1
-        
-        # the default previous space depends on whether we have prevoiusly finished a word 
-        if opt[i].lastspace is None:
-            opt[i].lastspace = opt[i-1].lastspace
-        else:
-            opt[i].lastspace = i-1
-
-        best_word_len = 0
-        prefixes.add(trie.root)
-        trials = len(prefixes)
-        
-        # look through every possible prefix and eliminate it if no longer viable
-        for _ in range(trials):
-            
-            prefix_node = prefixes.remove()
-            
-            # return to end of queue if still viable
-            if doc[i] in prefix_node.children:
-                prefix_child = prefix_node.children[doc[i]]
-                prefixes.add(prefix_child)
-                
-                # if we are currently at the end of a word
-                if prefix_child.is_end_of_word():
-                    word_len = prefix_child.depth
-                                        
-                    # first full word encountered
-                    if i-word_len < 0:
-                        opt[i].count = 0
-                        best_word_len = word_len
-                        opt[i].lastspace = -1
-                        if i < len(doc)-1:
-                            opt[i+1].lastspace = i
-                        
-                    # second or later full word encountered
-                    elif opt[i-word_len].count < opt[i].count or \
-                    (opt[i-word_len].count == opt[i].count and word_len > best_word_len):
-                        opt[i].count = opt[i-word_len].count
-                        best_word_len = word_len
-                        opt[i].lastspace = i-word_len
-                        if i < len(doc)-1:
-                            opt[i+1].lastspace = i
-
-    """                        
-    for i in range(len(doc)):
-        print(i,doc[i],opt[i].count,opt[i].lastspace)
+    Returns:
+        A list of OptObject instances.
     """
+    opt = [OptObject() for _ in range(len(doc))] 
     
-    return opt
+    # The "base case" is the index one past the end of doc, which has
+    # no spaces and no unrecognized characters. One extra object serves
+    # as "padding" removing the need for checks to avoid IndexError.
+    
+    opt.append(OptObject(0,len(doc)))
+      
+    i = len(doc)
+    while i > 0:
+        i -= 1
         
-test_trie = Trie()
-for word in ["hope","hopeless","hopelessly","less","hop","shop","sly","look","looked","fox","ox","foxes","sand","a","and","but","she","big","dog"]:
-    test_trie.insert(word)
+        # At each index i, the "default" count value is the result of 
+        # treating character at i as unrecognized. The "default" 
+        # nextword will already have been set if i+1 was found to mark
+        # the start of a word that minimzes unrecognized characters.
+        
+        opt[i].count = opt[i+1].count+1
+        if opt[i].nextword == None:
+            opt[i].nextword = opt[i+1].nextword
+            
+        j = i
+        cur_node = root        
+        while j < len(doc):
+            
+            # We terminate as early as possible when the trie does not
+            # support a substring beginning at i. Otherwise, we "grab"
+            # the longest possible word starting at i that lowers count
+            # if any such words exist.
+            
+            if not doc[j] in cur_node.children:
+                break
+            
+            cur_node = cur_node.children[doc[j]]
+            j += 1 
+            
+            if cur_node.is_end_of_word() and opt[j].count <= opt[i].count:
+                opt[i].count = opt[j].count
+                opt[i].nextword = j
+                if i > 0:
+                    opt[i-1].nextword = i
+    return opt                
+
+def build_string(opt,doc):
+    """Constructs the unconcatenated string.
+    
+    Args:
+        opt: A list of OptObject instances.
+        doc: A string of only lower-case characters.
+    
+    Returns:
+        A string of only lower-case characters and spaces.
+    """
+    builder = []
+    next_space = opt[0].nextword
+
+    for i in range(len(doc)):
+        if i == next_space:
+            builder.append(" ")
+            next_space = opt[i].nextword
+        builder.append(doc[i])
+
+    return "".join(builder)
+    
+def test():
+    """Tests some sample inputs. Far from exhaustive but covers basic 
+    "edge cases" and confirms algorithm behavior is not greedy."""
+    dict_a = ["hope","hopeless","hopelessly","less","hop","shop","sly","look",
+              "looked","fox","ox","foxes","sand","a","and","but","she","big",
+              "dog","at","the","openness","just","like","her","brother"]
+    
+    docs_a = ["",
+    
+              "a",
+              "x",
+    
+              "ax",  # word at start
+              "xa",  # word at end
+              "ox",  # full word
+              "xx",  # no word
+              
+              "atx",
+              "xat",
+              "dog",
+              "xax",
+              "xtx",
+              
+              "dogdog",
+              "dogxdog",
+              "dogxxdog",
+              
+              "hopenness",  # h<openness> better than <hope>nness
+              "opelessly",  # ope<less>ly better than opeles<sly>
+              "hopeless",
+              "hopelessly",
+              
+              "jesshopelesslylookedatthebigsandox",
+              "jesslookedhopelesslyliketimherbrother",
+              
+              "xmpqqqleszzz"]
+    
+    # More examples of behavior with compound words.
+    
+    dict_b = ["he","stood","and","stepped","out","sourcing","the","enemy",
+              "with","his","handheld","GPS","tracker","hand","held","a",
+              "red","ball","outsourcing","doorknob","door","knob","turned"]
+              
+    docs_b = ["hestoodandsteppedoutsourcingtheenemywithhishandheldgpstracker",
+              "hishandheldaredball",
+              "heturnedthedoorknob"]
+              
+    for docs, dictionary in [(docs_a,dict_a),(docs_b,dict_b)]:
+        for doc in docs:
+            print("In : ",doc)
+            print("Out: ",f1(doc,dictionary))
+            print("-" * 80)
