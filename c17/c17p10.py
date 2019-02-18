b7a164d21a44cbd7904b4d3d07b533211ce9524f
Jake Albert <albert.jacob.m@gmail.com>
2019-02-17 21:38:19 -0500

    Edit c17p10

--- c17/c17p10.py
+++ c17/c17p10.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 # c17p10
 
-#  Majority Element: A majority element is an element that makes up more than
+# Majority Element: A majority element is an element that makes up more than
 # half of the items in an array. Given a positive integers array, find the 
 # majority element. If there is no majority element, return -1. Do this in
 # O(N) time and 0(1) space.
